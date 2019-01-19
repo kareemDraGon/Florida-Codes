8b188ea95355d189871579d7322126d5564964f2
kareemDraGon <46831132+kareemDraGon@users.noreply.github.com>
2019-01-19 16:11:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -285,10 +285,7 @@ client.on('message', message => {
   let args = message.content.split(" ").slice(1);
 
   if (command == "kick") {
-               if(!message.channel.guild) return message.reply('** This command only for servers**');
-         
-  if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("**You Don't Have ` KICK_MEMBERS ` Permission**");
-  if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply("**I Don't Have ` KICK_MEMBERS ` Permission**");
+               if(!message.channel.guild) return message.reply('** This command only for servers**');         
   let user = message.mentions.users.first();
   let reason = message.content.split(" ").slice(2).join(" ");
   if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
